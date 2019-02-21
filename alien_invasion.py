979734f5af7597f3ed94fbb34cdde77633f2deff
namlt3820 <namlt3820@gmail.com>
2019-02-21 11:49:41 +0700

    First Edition

---
+++ alien_invasion.py
@@ -0,0 +1,42 @@
+import pygame
+import game_functions as gf
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scroreboard
+
+
+def main():
+	# Initialize settings and screen object.
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption('Alien Invasion')
+
+	# Make the Play button.
+	play_button = Button(ai_settings, screen, "Play")
+
+	# Create an instance to store game statistics and create a scoreboard.
+	stats = GameStats(ai_settings)
+	sb = Scroreboard(ai_settings, screen, stats)
+
+	# Make a ship, a group of bullets, and a group of aliens.
+	ship = Ship(ai_settings, screen)
+	bullets = Group()
+	aliens = Group()
+	
+	# Create the fleet of aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
+	# Star the main loop for the game.
+	while True:
+		gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+if __name__ == "__main__":
+	main()
